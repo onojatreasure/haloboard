3ebbc2254b13cde4c44b89c64bf72fd35c810dda
Onoja Treasure <onojatreasure@gmail.com>
2020-02-11 07:31:03 +0100

    added framework and started testing

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title></title>
+
+</head>
+
+<body>
+
+    <h1>Haloboard</h1>
+
+    <ul>
+        @foreach  ($projects as $project)
+            <li>{{ $project->title }}</li>
+        @endforeach
+    </ul>
+
+</body>
+
+</html>
+
+</html>
