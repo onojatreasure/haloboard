91a129c82f3524609d9a4794288ed57faa10291a
Onoja Treasure <onojatreasure@gmail.com>
2020-03-18 06:34:33 +0100

    Added subject to Activity

--- tests/Feature/TriggerActivityTest.php
+++ tests/Feature/TriggerActivityTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Project;
+use App\Task;
 use Facades\Tests\Setup\ProjectFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -34,40 +35,50 @@ class TriggerActivityTest extends TestCase
     }
 
     /** @test */
-    public function creating_a_new_task()
+    function creating_a_new_task()
     {
         $project = ProjectFactory::create();
 
-        $project->addTask('some task');
+        $project->addTask('Some task');
 
         $this->assertCount(2, $project->activity);
 
-        $this->assertEquals('created_task', $project->activity->last()->description);
+        tap($project->activity->last(), function ($activity) {
+            $this->assertEquals('created_task', $activity->description);
+            $this->assertInstanceOf(Task::class, $activity->subject);
+            $this->assertEquals('Some task', $activity->subject->body);
+        });
     }
 
     /** @test */
-    public function completing_a_task()
+    function completing_a_task()
     {
         $project = ProjectFactory::withTasks(1)->create();
 
-        $this->actingAs($project->owner)->patch($project->tasks[0]->path(), [
-            'body' => 'foobar',
-            'completed' => true
-        ]);
+        $this->actingAs($project->owner)
+            ->patch($project->tasks[0]->path(), [
+                'body' => 'foobar',
+                'completed' => true
+            ]);
 
         $this->assertCount(3, $project->activity);
-        $this->assertEquals('completed_task', $project->activity->last()->description);
+
+        tap($project->activity->last(), function ($activity) {
+            $this->assertEquals('completed_task', $activity->description);
+            $this->assertInstanceOf(Task::class, $activity->subject);
+        });
     }
 
     /** @test */
-    public function incompleting_a_task()
+    function incompleting_a_task()
     {
         $project = ProjectFactory::withTasks(1)->create();
 
-        $this->actingAs($project->owner)->patch($project->tasks[0]->path(), [
-            'body' => 'foobar',
-            'completed' => true
-        ]);
+        $this->actingAs($project->owner)
+            ->patch($project->tasks[0]->path(), [
+                'body' => 'foobar',
+                'completed' => true
+            ]);
 
         $this->assertCount(3, $project->activity);
 
@@ -76,7 +87,7 @@ class TriggerActivityTest extends TestCase
             'completed' => false
         ]);
 
-        $project = $project->fresh();
+        $project->refresh();
 
         $this->assertCount(4, $project->activity);
 
