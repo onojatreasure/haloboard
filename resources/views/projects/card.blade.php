7c5104843c70e12e43ccab57649c34c95ca3cd5b
Onoja Treasure <onojatreasure@gmail.com>
2020-04-02 06:36:57 +0100

    Tailwind themes using css variables

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -4,17 +4,17 @@
             class="font-normal
                 text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-light pl-4"
             >
-                <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a> 
+                <a href="{{ $project->path() }}" class="text-default no-underline">{{ $project->title }}</a> 
         </h3>
 
-    <div class="text-grey-dark mb-4 flex-1">{{ str_limit($project->description, 250) }}</div>
+    <div class="text-default mb-4 flex-1">{{ str_limit($project->description, 250) }}</div>
     
-    $can('manage', $project)
+    @can ('manage', $project)
         <footer>
             <form method="POST" action="{{ $project->path() }}" class="text-right">
                 @method('DELETE')
                 @csrf
-                <button class="text-xs" type="submit">Delete</button>
+                <button type="submit" class="text-xs">Delete</button>
             </form>
         </footer>
     @endcan
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <header class="flex items-center mb-3 pb-4">
         <div class="flex justify-between items-end w-full">
-            <p class="text-grey text-sm font-normal">
+            <p class="text-default text-sm font-normal">
                 <a href="/projects" 
-                    class="text-grey text-sm font-normal no-underline hover:underline">
+                    class="text-default text-sm font-normal no-underline hover:underline">
                         My Projects
                 </a>
                 / {{ $project->title }}
@@ -33,7 +33,7 @@
         <div class="lg:flex -mx-3">
             <div class="lg:w-3/4 px-3 mb-6">
                 <div class="mb-8">
-                    <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
+                    <h2 class="text-lg text-default font-normal mb-3">Tasks</h2>
 
                     {{-- tasks --}}
                     @foreach ($project->tasks as $task)
@@ -42,9 +42,9 @@
                                 @method('PATCH')
                                 @csrf
 
-                                <div class="flex">
-                                    <input name="body" value="{{ $task->body }}" class="w-full {{ $task->completed ? 'text-grey' : '' }}">
-                                    <input name="completed" type="checkbox" onChange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
+                                <div class="flex items-center">
+                                    <input name="body" value="{{ $task->body }}" class="bg-card text-default w-full {{ $task->completed ? 'text-default' : '' }}">
+                                    <input name="completed" class="bg-card text-default" type="checkbox" onChange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
                                 </div>
                             </form>
                         </div>
@@ -54,13 +54,13 @@
                         <form action="{{ $project->path() . '/tasks' }}" method="POST">
                             @csrf
 
-                            <input placeholder="Add a new task..." class="w-full" name="body">
+                            <input placeholder="Add a new task..." class="bg-card text-default w-full" name="body">
                         </form>
                     </div>
                 </div>
 
                 <div>
-                    <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
+                    <h2 class="text-lg text-default font-normal mb-3">General Notes</h2>
 
                     {{-- general notes --}}
                     <form method="POST" action="{{ $project->path() }}">
